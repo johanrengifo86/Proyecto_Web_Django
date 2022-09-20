8a6147b1068426fd4989766d3d40bd7b4801dc73
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-09-20 18:11:35 -0500

    envio de datos al formulario

--- contacto/views.py
+++ contacto/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import FormularioContacto 
 
 # Create your views here.
 
 def contacto(request):
     formulario_contacto = FormularioContacto()
+
+# Procedimiento para rescatar la informacion del formulario
+    if request.method == "POST": # si el metodo de la peticion es igual Post
+        formulario_contacto = FormularioContacto(data=request.POST) # carga la informacion del formulario en el constructor
+        if formulario_contacto.is_valid(): # si la informacion es valida
+            # Guarda en las variables la informacion 
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+        # Redirecciona a la pagina de contacto despues del envio de datos
+            return redirect("/contacto/?valido")
+
     return render(request, "contacto/contacto.html", {'miFormulario':formulario_contacto})
