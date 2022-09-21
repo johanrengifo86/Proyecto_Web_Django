55650c1da772898bc8288c30a32c9d1a0bf6aafa
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-09-21 18:02:38 -0500

    Creacion de modelos categoria y producto en models.py

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,34 @@
+from tabnanny import verbose
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+class CategoriaProducto(models.Model):
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True) # Inserta de forma automatica la fecha de creacion
+    updated = models.DateTimeField(auto_now_add=True) # Inserta de forma automatica la fecha de actualizacion
+
+    # Especifica el nombre en singular y plural la categoria  
+    class Meta:
+        verbose_name = "categoria_producto"
+        verbose_name_plural = "categorias_producto"
+
+    # Devuelve el nombre de la categoria 
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    # clave foranea, se especifica la tabla de relacion, y se especifica la eliminacion en cascada 
+    categorias = models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to= "tienda", null=True, blank=True)
+    precio = models.FloatField()
+    disponibilidad = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+
+    
