271acd0c65c8282e20a790db0b51440733fbce5d
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-09-22 12:52:07 -0500

    registro de modelos, edicion de views,templates app tienda

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CategoriaProducto, Producto
 
 # Register your models here.
+
+# Se especifica que los campos created y updated de los modelos son de solo lectura
+class CategoriaProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+# Registro de modelos 
+
+admin.site.register(CategoriaProducto, CategoriaProductoAdmin)
+admin.site.register(Producto, ProductoAdmin)
+
+
