34d55ea879e8924815c51d5b6f2f44dfae058117
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-09-30 10:47:57 -0500

    se añaden mensajes errror login

--- Proyecto_Web/settings.py
+++ Proyecto_Web/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.messages import constants as mensajes_de_error
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,6 +143,16 @@ EMAIL_HOST_PASSWORD ="ythrfzuoabqcoypg"
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+MESSAGE_TAGS = { 
+
+    mensajes_de_error.DEBUG: 'debug',
+    mensajes_de_error.INFO: 'info',
+    mensajes_de_error.SUCCESS: 'success',
+    mensajes_de_error.WARNING: 'warning',
+    mensajes_de_error.ERROR: 'danger',
+
+}
 
 
 # Default primary key field type
