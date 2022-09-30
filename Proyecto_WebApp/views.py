6a47a2cde3c87ab94db154387ebfa4a79eaa9557
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-09-30 11:36:25 -0500

    correccion errores app carrocompras

--- Proyecto_WebApp/views.py
+++ Proyecto_WebApp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
+from carroCompras.carro import Carro
+
 
 # Create your views here.
 # Vistas del Proyecto web 
 
 def home(request):
+
+    carro = Carro(request)
+
     return render(request, "Proyecto_WebApp/home.html")
 
 
--- carroCompras/context_processor.py
+++ carroCompras/context_processor.py
@@ -1,8 +1,13 @@
 
 def importe_total_carro(request):
     total = 0
-    #if request.user.is_authenticated:
-    if 'carro' in request.session:
+    if request.user.is_authenticated:
+    #if 'carro' in request.session:
         for key, value in request.session["carro"].items():
             total = total + (float(value["precio"]))
+
+    else:
+        total= 'Debes hacer login'
+
+        
     return{"importe_total_carro":total}
